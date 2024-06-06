ab28cf47519faf0b5cf0afa563f994653510b36d
ivangund <ivan.gundyrev@yandex.ru>
2024-06-06 23:56:42 +0300

    Добавить класс для подзадач сложных задач

---
+++ src/Subtask.java
@@ -0,0 +1,33 @@
+public class Subtask extends Task {
+
+    private int epicId;
+
+    public Subtask(String title, String description, int epicId) {
+        super(title, description);
+        this.epicId = epicId;
+    }
+
+    public Subtask(int id, String title, String description, Status status, int epicId) {
+        super(id, title, description, status);
+        this.epicId = epicId;
+    }
+
+    public int getEpicId() {
+        return epicId;
+    }
+
+    public void setEpicId(int epicId) {
+        this.epicId = epicId;
+    }
+
+    @Override
+    public String toString() {
+        return "Subtask{" +
+                "epicId=" + epicId +
+                ", id=" + getId() +
+                ", title='" + getTitle() + '\'' +
+                ", description='" + getDescription() + '\'' +
+                ", status=" + getStatus() +
+                '}';
+    }
+}
