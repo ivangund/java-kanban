4c26e09dbbc8825a0ef5d37559437fb6b48e98ed
ivangund <ivan.gundyrev@yandex.ru>
2024-06-06 23:56:07 +0300

    Добавить класс для сложных задач

---
+++ src/Epic.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Epic extends Task {
+
+    private final List<Integer> subtaskIds;
+
+    public Epic(String title, String description) {
+        super(title, description);
+        this.subtaskIds = new ArrayList<>();
+    }
+
+    public List<Integer> getSubtaskIds() {
+        return subtaskIds;
+    }
+
+    public void addSubtaskId(int subtaskId) {
+        subtaskIds.add(subtaskId);
+    }
+
+    public void removeSubtaskId(int subtaskId) {
+        subtaskIds.remove(Integer.valueOf(subtaskId));
+    }
+
+    @Override
+    public String toString() {
+        return "Epic{" +
+                "subtaskIds=" + subtaskIds +
+                ", id=" + getId() +
+                ", title='" + getTitle() + '\'' +
+                ", description='" + getDescription() + '\'' +
+                ", status=" + getStatus() +
+                '}';
+    }
+}
