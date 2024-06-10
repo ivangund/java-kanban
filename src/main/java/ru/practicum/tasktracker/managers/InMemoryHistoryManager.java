acb35ac4bd90e7ef1413ea7f69ee41dff6248d0a
Ivan Gundyrev <ivan.gundyrev@yandex.ru>
2024-06-11 00:42:25 +0300

    Вынести максимальную длину истории в константу

--- src/main/java/ru/practicum/tasktracker/managers/InMemoryHistoryManager.java
+++ src/main/java/ru/practicum/tasktracker/managers/InMemoryHistoryManager.java
@@ -6,11 +6,13 @@ import main.java.ru.practicum.tasktracker.tasks.Task;
 
 public class InMemoryHistoryManager implements HistoryManager {
 
+    private static final int MAX_HISTORY_SIZE = 10;
+
     private final List<Task> history = new ArrayList<>();
 
     @Override
     public void add(Task task) {
-        if (history.size() >= 10) {
+        if (history.size() >= MAX_HISTORY_SIZE) {
             history.removeFirst();
         }
         history.add(task);
