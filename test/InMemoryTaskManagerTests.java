b235e6c4dd0bf682cbe27fbfbfa384936a32d823
ivangund <ivan.gundyrev@yandex.ru>
2024-07-22 03:16:11 +0300

    Изменить модификатор доступа taskManager

--- test/InMemoryTaskManagerTests.java
+++ test/InMemoryTaskManagerTests.java
@@ -12,7 +12,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class InMemoryTaskManagerTests {
 
-    private TaskManager taskManager;
+    protected TaskManager taskManager;
 
     @BeforeEach
     void setUp() {
